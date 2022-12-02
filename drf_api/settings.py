e3c844ba1c0915ce76f11ecd505b5d4d0be05b70
CozyPlantlady <laurajohanna.taipale@gmail.com>
2022-12-02 18:02:14 +0000

    Allowed_hosts and client_origin_dev

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import re
 from pathlib import Path
 import os
 import dj_database_url
@@ -61,7 +62,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['localhost', 'api-practice-for-cozy.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', os.environ.get('ALLOWED_HOST'),]
 
 
 # Application definition
