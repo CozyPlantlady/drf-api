f21380aa286ce177e726497b29f9200c6e16a6d9
CozyPlantlady <laurajohanna.taipale@gmail.com>
2022-12-04 13:15:44 +0000

    auth

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -62,7 +62,9 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['localhost', os.environ.get('ALLOWED_HOST'),]
+ALLOWED_HOSTS = ['localhost', os.environ.get('ALLOWED_HOST'), ]
+CSRF_TRUSTED_ORIGINS = [os.environ.get(
+                                      'CLIENT_ORIGIN_DEV', 'CLIENT_ORIGIN', )]
 
 
 # Application definition
@@ -106,9 +108,11 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     ]
 
-if 'CLIENT_ORIGIN' in os.environ:
-    CORS_ALLOWED_ORIGINS = [
-        os.environ.get('CLIENT_ORIGIN')
+if 'CLIENT_ORIGIN_DEV' in os.environ:
+    extracted_url = re.match(r'^.+-', os.environ.get(
+                             'CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
+    CORS_ALLOWED_ORIGIN_REGEXES = [
+        rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
     ]
 else:
     CORS_ALLOWED_ORIGIN_REGEXES = [
