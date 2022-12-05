9f7cc763777db6c7db5b5ef9e7afb4536fabf5cc
CozyPlantlady <laurajohanna.taipale@gmail.com>
2022-12-05 17:30:54 +0000

    Corrected typo on profiles, views

--- profiles/views.py
+++ profiles/views.py
@@ -13,8 +13,8 @@ class ProfileList(generics.ListAPIView):
     """
     queryset = Profile.objects.annotate(
         posts_count=Count('owner__post', distinct=True),
-        followers_count=Count('owner__followed', dinstinct=True),
-        following_count=Count('owner__following', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True)
     ).order_by('-created_at')
     serializer_class = ProfileSerializer
     filter_backends = [
@@ -41,7 +41,7 @@ class ProfileDetail(generics.RetrieveUpdateAPIView):
     permission_classes = [IsOwnerOrReadOnly]
     queryset = Profile.objects.annotate(
         posts_count=Count('owner__post', distinct=True),
-        followers_count=Count('owner__followed', dinstinct=True),
-        following_count=Count('owner__following', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True)
     ).order_by('-created_at')
     serializer_class = ProfileSerializer
