9a6198b65defe0f55ddf17f14b26ebd9b79f6c69
CozyPlantlady <laurajohanna.taipale@gmail.com>
2022-12-05 16:56:51 +0000

    Settings changed

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -118,6 +118,17 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+if 'CLIENT_ORIGIN' in os.environ:
+    CORS_ALLOWED_ORIGINS = [
+        os.environ.get('CLIENT_ORIGIN')
+    ]
+else:
+    CORS_ALLOWED_ORIGIN_REGEXES = [
+        r"^https://.*\.gitpod\.io$",
+    ]
+
+CORS_ALLOW_CREDENTIALS = True
+
 ROOT_URLCONF = 'drf_api.urls'
 
 TEMPLATES = [
